b186d3904017de39b3ac8c513557e4e72676e1b9
habib <habib@DESKTOP-HIPNQGL>
2025-07-12 00:08:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignmentc_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
